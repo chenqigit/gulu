00b2389ddda0bc5d784de8a482c35efea66e5f1c
jcq <41006328@qq.com>
2020-05-22 23:50:16 +0800

    将icon代码整理到icon.vuee

---
+++ src/icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .g-icon {
+    width: 1rem;
+    height: 1rem;
+  }
+</style>
