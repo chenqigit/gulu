d9c5c55e63f59c7eedb0fdbdd2bb8bfb0ce26c09
jcq <41006328@qq.com>
2020-05-23 11:06:28 +0800

    检测g-button-group的子元素是否是buttonn

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,7 +6,15 @@
 
 <script>
 export default {
-
+  mounted() {
+    console.log(this.$el.children)
+    for(let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase()
+      if(name !== 'button') {
+        console.log(`g-button-group的子元素应该全是 g-button，你写的是${name}`)
+      }
+    }
+  }
 }
 </script>
 
